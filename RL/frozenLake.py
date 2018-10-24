ede6c3833e010a7b7dee95a2c8d3612ee8520a1e
krishnaw14 <krishnaw14@gmail.com>
2018-10-25 02:14:29 +0530

    Q Learning frozenLake

--- RL/frozenLake.py
+++ RL/frozenLake.py
@@ -1,12 +1,4 @@
-# Using Q-Learning in Frozen Lake Environment
-
-import gym
-import random
-import numpy as np 
-from collections import namedtuple
-import collections
-import matplotlib.pyplot as plt 
-
+# Using Q-Learning in FrozenLake Environment
 
 '''
 FrozenLake-v0 Environment
@@ -25,120 +17,120 @@ HFFG       (G: goal, where the frisbee is located)
 The episode ends when you reach the goal or fall in a hole. You receive a reward of 1 if you reach the goal, and zero otherwise.
 
 More details can be found here - https://gym.openai.com/envs/FrozenLake-v0/
+
+In a nutshell, the goal of the game is to go from start state (S) to goal state (G) by walking just on the frozen states (F) and avoid holes (H). 
 '''
 
-# Basic Functions to choose an action following different policies
+import numpy as np 
+import gym
+import random
 
-# Select the action using a epsilon greedy approach 
-def select_eps_greedy_action(table, obs, n_actions):
-	value, action = best_action_value(table, obs)
+# Creating the environment
+env = gym.make("FrozenLake-v0")
 
-	if random.random() < epsilon:
-		return random.randint(0, n_actions-1)
-	else:
-		return action
+# Create the Q table
 
-# Select an action using greedy policy (take the best action according to the policy)
-def select_greedy_action(table, obs, n_actions):
-	value, action = best_action_value(table,obs)
-	return action
+'''
+Q-table comprises maximum expected reward for each action at each stage. 
+The rows of the table correspond to the states while the columns correspond to the actions that the agent can take under the given policy.
+Each value in the Q-table (Q-table score) is the maximum expected future reward that I'll get if I take that action at that stage with 
+the given best policy.
+'''
 
-# Select the best action to maximise action value function
-def best_action_value(table, state):
-	best_action = 0
-	max_value = 0
-	for action in range(n_actions):
-		if table[(state,action)]>max_value:
-			best_action = action
-			max_value = table[(state,action)]
+action_size = env.action_space.n # Possible number of states (= 16 for frozenLake environment)
+state_size = env.observation_space.n # Possible number of actions (= 4 for frozenLake environment)
 
-	return max_value, best_action
-
-# Update Q(obs0, action) according to Q(obs1, *) and the reward just obtained
-def Q_learning(table, obs0, obs1, reward, action):
-
-	# Take the best value reachable from state obs1
-	best_value, _ = best_action_value(table, obs1)
-	# Calculate Q-target value
-	Q_target = reward + gamma*best_value
-	# Calculate Q-error between target and previous value
-	Q_error = Q_target - table[(obs0, action)]
-	#Update Q(obs0, action)
-	table[(obs0, action)] += learning_rate*Q_error
-
-# Test the new table playing test_episodes games
-def test_game(env, table, n_actions):
-	reward_games = []
-	for _ in range(test_episodes):
-		obs = env.reset()
-		rewards = 0
-		while True:
-			# env.render() 
-			# Render can be uncommented to visualize the path taken y the agent in each game
-			next_obs, reward, done, _ = env.step(select_greedy_action(table, obs, n_actions))
-			obs = next_obs
-			rewards += reward
-
-			if done:
-				reward_games.append(rewards)
-				break
-
-	return np.mean(reward_games)
+Qtable = np.zeros((state_size, action_size))
 
 # HyperParameters
-gamma = 0.95
-epsilon = 1
-eps_decay_rate = 0.9993
+total_episodes = 15000
 learning_rate = 0.8
-test_episodes = 100
-max_games = 15001
+max_steps = 99 # Maximum steps that the agent can take per episode
+gamma = 0.95 # Discount rate
 
-# Create the Environment
-env = gym.make("FrozenLake-v0")
-obs = env.reset()
+# Exploration parameters
+epsilon = 1.0 # Exploration rate at the start - is high as initially we want our agent to explore the environment as much as possible
+max_epsilon = 1.0
+min_epsilon = 0.01 # Minimum exploration probability 
+decay_rate = 0.005 # Exponential decay rate for exploration rate
 
-obs_length = env.observation_space.n
-n_actions = env.action_space.n
+# Implementing the Q learning algorithm
 
-reward_count = 0
-games_count = 0
+rewards = [] # List of rewards
 
-# Create and initialize the table with 0.0
-table = collections.defaultdict(float)
-    
-test_rewards_list = []
+for episode in range(total_episodes):
 
-while games_count < max_games:
+	# Reset the environment and parameters before each episode
+	state = env.reset() 
+	step = 0
+	done = False
+	total_reward = 0
 
-    # Select the action following an ε-greedy policy
-    action = select_eps_greedy_action(table, obs, n_actions)
-    next_obs, reward, done, _ = env.step(action)
+	for step in range(max_steps):
+		tradeoff = random.uniform(0,1) # Represents the tradeoff between exploration and exploitation
 
-    # Update the Q-table
-    Q_learning(table, obs, next_obs, reward, action)
+		if tradeoff > epsilon: # We exploit the environment in this case chosing the best action for that state (greedy approach)
+			action = np.argmax(Qtable[state,:])
 
-    reward_count += reward
-    obs = next_obs
+		else: # We explore the environment in this case chosing any random action
+			action = env.action_space.sample()
 
-    if done:
-        epsilon *= eps_decay_rate
+		# Take the action and obtain the reward and new state for this action
+		new_state, reward, done, info = env.step(action)
 
-        # Test the new table every 1000 games
-        if games_count % 1000 == 0:
-            test_reward = test_game(env, table, n_actions)
-            print('Ep:', games_count, 'Test reward:', test_reward, np.round(epsilon,2))
+		# Update Q table using Bellman Ford equation
+		Qtable[state,action] = Qtable[state,action] + learning_rate*(reward + gamma*np.max(Qtable[new_state, :]) - Qtable[state,action] )
 
-            test_rewards_list.append(test_reward)
+		# Update the state and reward
+		total_reward = total_reward + reward
+		state = new_state
 
-        obs = env.reset()
-        reward_count = 0
-        games_count += 1    
+		# If the episode is ended
+		if done == True:
+			break
 
-# Plot the accuracy over the number of steps
-# print("Final Q value table", table)
+	# Update epsilon as we aim to increase exploitation and reduce exploration as the training proceeds because the Q table starts to take shape after every episode iteration.
+	epsilon = min_epsilon + (max_epsilon - min_epsilon)*np.exp(-decay_rate*episode) 
+	rewards.append(total_reward)
 
-plt.figure(figsize=(10,10))
-plt.xlabel('Steps')
-plt.ylabel('Accurracy')
-plt.plot(test_rewards_list)
-plt.show()
+
+print("Average score over training:", np.mean(np.array(rewards))) 
+print("Final Q-table after training:", Qtable)
+
+
+# Testing the algorithm
+print("Testing the algorithm on 5 episodes...")
+
+env.reset()
+for episode in range(5):
+	state = env.reset()
+	step = 0
+	done = False
+	print("---------- Episode",episode, "---------")
+
+	for step in range(max_steps):
+		action = np.argmax(Qtable[state,:])
+		new_state, reward, done, info = env.step(action)
+
+		state = new_state
+
+		if done:
+			env.render() # To verify that our agent reaches the goal state
+			print("Number of steps = ", step)
+			break
+
+env.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
